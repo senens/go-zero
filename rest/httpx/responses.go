03a02507b5dbbccc8c5ed80d6ad7f720de8ed61f
long <wudiqingcha@163.com>
2024-10-21 13:50:14 +0800

    secret

--- rest/httpx/responses.go
+++ rest/httpx/responses.go
@@ -1,7 +1,12 @@
 package httpx
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"reflect"
 	"sync"
@@ -22,6 +27,10 @@ type RespJsonStruct struct {
 	T    int         `json:"t"`
 	V    string      `json:"v"`
 	Data interface{} `json:"data"`
+}
+
+type RespEncrypt struct {
+	Data string `json:"data"`
 }
 
 type RespValue struct {
@@ -100,7 +109,7 @@ func WriteJson(w http.ResponseWriter, code int, v interface{}) {
 //自定义返回参数模板
 //
 // Error writes err into w.
-func RespJsonError(w http.ResponseWriter, err error) {
+func RespJsonError(w http.ResponseWriter, err error, secret string) {
 	lock.RLock()
 	handler := errorHandler
 	lock.RUnlock()
@@ -115,17 +124,19 @@ func RespJsonError(w http.ResponseWriter, err error) {
 	if ok {
 		http.Error(w, e.Error(), code)
 	} else {
-		WriteJson(w, http.StatusOK, RespJsonStruct{
+		dataType, _ := json.Marshal(RespJsonStruct{
 			Code: code,
 			Msg:  "",
 			T:    t,
 			V:    "1.0",
 			Data: body,
 		})
+		str, _ := encrypt(string(dataType), secret)
+		WriteJson(w, http.StatusOK, str)
 	}
 }
 
-func RespJson(w http.ResponseWriter, err error, v interface{}, code int, msg string, version string) {
+func RespJson(w http.ResponseWriter, err error, v interface{}, code int, msg string, version, secret string) {
 	t := int(time.Now().Unix())
 	if code == 0 {
 		code = 200
@@ -137,14 +148,16 @@ func RespJson(w http.ResponseWriter, err error, v interface{}, code int, msg str
 	if len(version) == 0 {
 		version = "1.0"
 	}
-
-	WriteJson(w, http.StatusOK, RespJsonStruct{
+	dataType, _ := json.Marshal(RespJsonStruct{
 		Code: code,
 		Msg:  msg,
 		T:    t,
 		V:    version,
 		Data: v,
 	})
+	str, _ := encrypt(string(dataType), secret)
+
+	WriteJson(w, http.StatusOK, str)
 }
 
 func IsNil(i interface{}) bool {
@@ -153,4 +166,77 @@ func IsNil(i interface{}) bool {
 		return vi.IsNil()
 	}
 	return false
+}
+
+// PKCS7Padding pads the plaintext to a block size using PKCS#7 padding.
+func pkcs7Pad(src []byte) []byte {
+	padding := aes.BlockSize - len(src)%aes.BlockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+// PKCS7Unpadding removes the PKCS#7 padding from the decrypted text.
+func pkcs7Unpad(src []byte) []byte {
+	length := len(src)
+	if length == 0 {
+		return src
+	}
+	padding := int(src[length-1])
+	if padding > length {
+		return src
+	}
+	return src[:(length - padding)]
+}
+
+// Encrypt encrypts the source string using AES in ECB mode.
+func encrypt(sSrc string, sKey string) (string, error) {
+	if sKey == "" {
+		return "", fmt.Errorf("Key为空null")
+	}
+	if sSrc == "" {
+		return "", nil
+	}
+
+	block, err := aes.NewCipher([]byte(sKey))
+	if err != nil {
+		return "", err
+	}
+
+	// Apply PKCS#7 padding to the source string.
+	src := pkcs7Pad([]byte(sSrc))
+
+	// Initialize an ECB mode encryption with a zero IV.
+	iv := make([]byte, aes.BlockSize)
+	ciphertext := make([]byte, len(src))
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext, src)
+
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+// Decrypt decrypts the source string using AES in ECB mode.
+func decrypt(sSrc string, sKey string) (string, error) {
+	if sKey == "" {
+		return "", fmt.Errorf("Key为空null")
+	}
+
+	block, err := aes.NewCipher([]byte(sKey))
+	if err != nil {
+		return "", err
+	}
+
+	// Decode the Base64 encoded string.
+	decoded, err := base64.StdEncoding.DecodeString(sSrc)
+	if err != nil {
+		return "", err
+	}
+
+	// Initialize an ECB mode decryption with a zero IV.
+	iv := make([]byte, aes.BlockSize)
+	plaintext := make([]byte, len(decoded))
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(plaintext, decoded)
+
+	// Remove the PKCS#7 padding from the decrypted text.
+	return string(pkcs7Unpad(plaintext)), nil
 }
